42d545e79ac3d4d6f75b321f2bb5d3828ef0f1ba
jnikita19 <Nikita.Joshi.snaatak@mygurukulam.co>
2025-08-14 15:04:39 +0530

    updated files

--- example/output.tf
+++ example/output.tf
@@ -1,206 +1,160 @@
+
 output "vpc_id" {
-  description = "The ID of the VPC"
   value       = module.network.vpc_id
+  description = "ID of the VPC"
 }
 
 output "vpc_cidr_block" {
-  description = "The CIDR block of the VPC"
   value       = module.network.vpc_cidr_block
+  description = "CIDR block of the VPC"
 }
 
-output "default_security_group_id" {
-  description = "The ID of the default security group for the VPC"
-  value       = module.network.default_security_group_id
-}
-
-output "default_network_acl_id" {
-  description = "The ID of the default network ACL"
-  value       = module.network.default_network_acl_id
-}
-
-output "default_route_table_id" {
-  description = "The ID of the default route table"
-  value       = module.network.default_route_table_id
-}
 
 output "igw_id" {
-  description = "The ID of the Internet Gateway"
   value       = module.network.igw_id
+  description = "Internet Gateway ID"
 }
 
-output "public_route_table_id" {
-  description = "The ID of the public route table"
-  value       = module.network.public_route_table_id
-}
-
-output "public_subnets" {
-  description = "List of public subnet IDs"
-  value       = module.network.public_subnets_ids
-}
-
-output "public_subnets_cidr_blocks" {
-  description = "CIDR blocks of public subnets"
-  value       = module.network.public_subnets_cidr_blocks
-}
-
-output "route53_zone_id" {
-  description = "Private Route53 zone ID"
-  value       = module.network.route53_zone_id
-}
-
-output "private_subnets" {
-  description = "List of private subnet IDs"
-  value       = module.network.private_subnets_ids
-}
-
-output "private_subnets_cidr_blocks" {
-  description = "CIDR blocks of private subnets"
-  value       = module.network.private_subnets_cidr_blocks
-}
-
-output "private_route_table_id" {
-  description = "List of private route table IDs"
-  value       = module.network.private_route_table_id
-}
-
-output "nat_gateway_ips" {
-  description = "List of NAT Gateway IP addresses"
-  value       = module.network.nat_gateway_ips
-}
-
-output "nat_gateway_id" {
+output "nat_gateway_ids" {
+  value       = module.network.nat_gateway_ids
   description = "List of NAT Gateway IDs"
-  value       = module.network.nat_gateway_id
 }
 
-output "database_subnets" {
-  description = "List of database subnet IDs"
-  value       = module.network.database_subnets_ids
+output "public_rt_id" {
+  value       = module.network.public_rt_id
+  description = "Public route table ID"
 }
 
-output "database_subnets_cidr_blocks" {
-  description = "CIDR blocks of database subnets"
-  value       = module.network.database_subnets_cidr_blocks
+output "private_rt_id" {
+  value       = module.network.privat_rt_id
+  description = "Private route table ID"
 }
 
 output "flow_logs_bucket_arn" {
-  description = "ARN of the S3 bucket for flow logs"
   value       = module.network.flow_logs_bucket_arn
+  description = "ARN of the Flow Logs S3 bucket"
 }
 
 output "vpc_flow_log_arn" {
-  description = "ARN of the VPC flow log resource"
   value       = module.network.vpc_flow_log_arn
+  description = "ARN of the VPC flow log"
 }
 
-output "public_nacl_id" {
-  value = module.network.public_nacl_id
+output "route53_zone_id" {
+  value       = module.network.route53_zone_id
+  description = "Private Route53 Zone ID"
 }
 
-output "private_nacl_id" {
-  value = module.network.private_nacl_id
+######################
+# VPC Endpoints
+######################
+
+output "s3_endpoint" {
+  value       = module.network.s3_endpoint
+  description = "S3 VPC endpoint details"
 }
 
-output "database_nacl_id" {
-  value = module.network.database_nacl_id
+output "ec2_endpoint" {
+  value       = module.network.ec2_endpoint
+  description = "EC2 VPC endpoint details"
 }
 
-
-
-output "s3_endpoint_id" {
-  description = "The ID of the S3 VPC endpoint"
-  value       = var.enable_s3_endpoint ? module.network.s3_endpoint.id : null
+output "nlb_endpoint" {
+  value       = module.network.nlb_endpoint
+  description = "NLB VPC endpoint details"
 }
 
-output "s3_endpoint_dns_entries" {
-  description = "DNS entries for the S3 VPC endpoint"
-  value       = var.enable_s3_endpoint ? module.network.s3_endpoint.dns_entries : null
-}
-
-output "ec2_endpoint_id" {
-  description = "The ID of the EC2 VPC endpoint"
-  value       = var.enable_ec2_endpoint ? module.network.ec2_endpoint.id : null
-}
-
-output "ec2_endpoint_dns_entries" {
-  description = "DNS entries for the EC2 VPC endpoint"
-  value       = var.enable_ec2_endpoint ? module.network.ec2_endpoint.dns_entries : null
-}
-
-output "nlb_endpoint_id" {
-  description = "The ID of the NLB VPC endpoint"
-  value       = var.enable_nlb_endpoint ? module.network.nlb_endpoint.id : null
-}
-
-output "nlb_endpoint_dns_entries" {
-  description = "DNS entries for the NLB VPC endpoint"
-  value       = var.enable_nlb_endpoint ? module.network.nlb_endpoint.dns_entries : null
-}
-
-output "endpoint_security_group_id" {
-  description = "The ID of the endpoint security group"
-  value       = var.enable_endpoint_sg ? module.network.endpoint_security_group.id : null
-}
-
-# Additional useful outputs
-output "all_vpc_endpoint_ids" {
-  description = "Map of all created VPC endpoint IDs"
-  value = {
-    s3  = var.enable_s3_endpoint ? module.network.s3_endpoint.id : null
-    ec2 = var.enable_ec2_endpoint ? module.network.ec2_endpoint.id : null
-    nlb = var.enable_nlb_endpoint ? module.network.nlb_endpoint.id : null
-  }
-}
-
-output "endpoint_sg_ingress_rules" {
-  description = "List of ingress rules for the endpoint security group"
-  value       = var.enable_endpoint_sg ? module.network.endpoint_security_group_rules.ingress : null
-}
-
-output "endpoint_sg_egress_rules" {
-  description = "List of egress rules for the endpoint security group"
-  value       = var.enable_endpoint_sg ? module.network.endpoint_security_group_rules.egress : null
-}
-
-
-
-
-###################
-output "alb_dns_name" {
-  value = module.network.alb_dns_name
-}
+######################
+# ALB Outputs
+######################
 
 output "alb_arn" {
-  value = module.network.alb_arn
+  value       = module.network.alb_arn
+  description = "ARN of the ALB"
+}
+
+output "alb_dns_name" {
+  value       = module.network.alb_dns_name
+  description = "DNS name of the ALB"
 }
 
 output "alb_zone_id" {
-  value = module.network.alb_zone_id
+  value       = module.network.alb_zone_id
+  description = "Zone ID of the ALB"
 }
 
 output "alb_http_listener_arn" {
-  value = module.network.alb_http_listener_arn
+  value       = module.network.alb_http_listener_arn
+  description = "HTTP Listener ARN for ALB"
 }
 
 output "alb_https_listener_arn" {
-  value = module.network.alb_https_listener_arn
+  value       = module.network.alb_https_listener_arn
+  description = "HTTPS Listener ARN for ALB"
 }
 
-output "alb_security_group_id" {
-  value = module.network.alb_security_group_id
-}
+######################
+# NLB Outputs
+######################
 
-output "alb_security_group_arn" {
-  value = module.network.alb_security_group_arn
-}
-
-
-output "web_sg_id" {
-  value = var.enable_public_web_security_group_resource ? module.nlb_security_group[0].sg_id : null
-}
-
-
-output "network_nlb_arn" {
+output "nlb_arn" {
   value       = module.network.nlb_arn
-  description = "NLB ARN from the network module"
+  description = "ARN of the NLB"
+}
+
+######################
+# Security Group Outputs
+######################
+
+output "alb_sg_id" {
+  description = "Security Group ID for ALB"
+  value       = module.alb_security_group.sg_id
+}
+
+output "nlb_sg_id" {
+  description = "Security Group ID for NLB"
+  value       = module.nlb_security_group.sg_id
+}
+
+output "endpoint_sg_id" {
+  description = "Security Group ID for Endpoint"
+  value       = module.endpoint_security_group.sg_id
+}
+
+
+###key pair##########
+
+output "created_key_pair_name" {
+  value = module.network.key_pair_name
+}
+
+output "generated_private_key_path" {
+  value = module.network.private_key_path
+}
+
+output "subnet_ids" {
+  value       = module.network.subnet_ids
+  description = "Map of subnet names to subnet IDs"
+}
+
+
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = module.network.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = module.network.private_subnet_ids
+}
+
+output "application_subnet_ids" {
+  description = "List of application subnet IDs"
+  value       = module.network.application_subnet_ids
+}
+
+output "database_subnet_ids" {
+  description = "List of database subnet IDs"
+  value       = module.network.database_subnet_ids
 }
